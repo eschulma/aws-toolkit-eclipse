423179d3025e1cbd8ddd927763c876fa3e8dae04
Erica S. Kane <ekane@sentrylink.com>
2021-02-05 14:35:58 -0500

    Removed deprecated code in egit-related classes.

--- bundles/com.amazonaws.eclipse.core/src/com/amazonaws/eclipse/core/egit/ui/SourceBranchPage.java
+++ bundles/com.amazonaws.eclipse.core/src/com/amazonaws/eclipse/core/egit/ui/SourceBranchPage.java
@@ -349,8 +349,8 @@ public class SourceBranchPage extends WizardPage {
             final Repository db = FileRepositoryBuilder
                     .create(new File("/tmp")); //$NON-NLS-1$
             int timeout = Activator.getDefault().getPreferenceStore().getInt(
-                    UIPreferences.REMOTE_CONNECTION_TIMEOUT);
-            listRemoteOp = new ListRemoteOperation(db, uri, timeout);
+                UIPreferences.REMOTE_CONNECTION_TIMEOUT);            
+			listRemoteOp = new ListRemoteOperation(db, uri, timeout);
             if (credentials != null)
                 listRemoteOp
                         .setCredentialsProvider(new EGitCredentialsProvider(
